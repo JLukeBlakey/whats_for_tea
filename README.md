16dc5e60082de60042ec2a53db1e958f89ad843e
luke <luke.blakey@gmail.com>
2025-02-28 17:12:40 +0000

    migrate from github

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# whats_for_tea
+# What's for tea?
+Based on inputs provided in `recipes.yaml`, creates a schedule of 5 random meals with a shopping list. 'regulars' are added to the list. 
 
+## Setup
+Files required:
+- `recipes.yaml` contains meals and key-value pairs of ingredients-quantity. 
+- `regulars.txt` text items to be added to the shopping list.
+-  `.env.emails` to contain key-value pairs, where the values are email addresses the list will be sent to. The key does not matter functionality-wise. See `.env.emails.sample` for an example.
+- `.env`  with `smtp_ip` defined, which should be the hostname or IP of the mail server to be used as a relay for sending out mail.
+
+If you do not have a mail server available, please message me with your hostname or IP (must be static) and I can add an allow rule for my mail server to be used.
---
+++ main.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import yaml
+import random
+import smtplib
+from email.message import EmailMessage
+import requests
+from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+recipients = {
+    **dotenv_values(".env.emails")
+}
+hc_url = os.getenv('HC_URL')
+smtp_ip = os.getenv('SMTP_IP')
+meals = []
+shopping_list = {}
+recipes = yaml.safe_load(open("recipes.yaml"))
+regulars = open("regulars.txt")
+kristoff_open = "Bonjour, Kristoff here. Here's your shopping plan for this week:\n"
+oddbox_data = []
+
+
+def create_meal_plan():
+    while len(meals) != 5:
+        random_meal = random.choice(list(recipes))
+        if random_meal not in meals:
+            meals.append(random_meal)
+    schedule = "\nMonday: " +  meals[0] + "\nTuesday: " + meals[1] + "\nWednesday: " + meals[2] + "\nThursday: " + meals[3] + "\nFriday: " + meals[4] + "\n"
+
+    return schedule
+
+
+def create_shopping_list(meals):
+    for meal in meals:
+        for ingredient, quantity in recipes[meal].items():
+            if ingredient in shopping_list and shopping_list[ingredient][1] == quantity[1]:
+                shopping_list[ingredient][0] = shopping_list[ingredient][0] + quantity[0]
+            else:
+                shopping_list[ingredient] = []
+                shopping_list[ingredient].append(quantity[0])
+                shopping_list[ingredient].append(quantity[1])
+
+    # convert to readable string
+    shopping_list_string = ""
+    for k, v in sorted(shopping_list.items()):
+        shopping_list_string = shopping_list_string + k + " (" + str(v[0]) + " " + v[1] + ")\n"
+
+    return shopping_list_string
+
+
+def email(content):
+    for address in recipients:
+        msg = EmailMessage()
+        msg.set_content(content)
+        msg["Subject"] = "Weekly Shop"
+        msg["From"] = "kristoff@cyclingpenguin.uk"
+        msg["To"] = recipients[address]
+        mail = smtplib.SMTP(smtp_ip)
+        mail.send_message(msg)
+        mail.quit
+
+
+def main():
+    content = kristoff_open \
+          + create_meal_plan() \
+          + "\nIngredients:\n" \
+          + create_shopping_list(meals) \
+          + "\nDon't forget the regulars!\n" \
+          + regulars.read() \
+          + "\nKristoff out."
+
+    email(content)
+
+    if hc_url:
+        requests.get(hc_url)
+
+
+main()
